ce6ba0035982214731d43eef61ae1dc4293e84ca
Simon Risberg <sirsimon40@gmail.com>
2020-04-26 15:07:35 +0200

    Removing more käbbel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return (<div>Hello world</div>);
 }
 
 export default App;
