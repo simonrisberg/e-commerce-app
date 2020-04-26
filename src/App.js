b13db3b5084dfc50592ee50b4ef7b76031640ccb
Simon Risberg <sirsimon40@gmail.com>
2020-04-26 15:58:55 +0200

    Added general CSS layout for the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
-  return (<div>Hello world</div>);
+  return (
+  
+    <div>
+      <HomePage />
+    </div>
+    
+  );
 }
 
 export default App;
