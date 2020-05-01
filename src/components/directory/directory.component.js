9b691c8ccfafde4477bfb05c3361ff16a5136db6
Simon Risberg <sirsimon40@gmail.com>
2020-05-01 12:25:19 +0200

    Added an image background for the different menu items

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -11,27 +11,29 @@ class Directory extends React.Component {
         this.state = {
             sections: [{
                 title: 'hats',
-                imageUrl: 'htttps://i.ibb.co/cvpntL1/hats.png',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1
             },
             {
                 title: 'jackets',
-                imageUrl: 'htttps://i.ibb.co/px2tCc3/jackets.png',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                 id: 2 
             },
             {
                 title: 'sneakers',
-                imageUrl: 'htttps://i.ibb.co/0jqHpnp/sneakers.png',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                 id: 3 
             },
             {
                 title: 'womens',
-                imageUrl: 'htttps://i.ibb.co/GCCdy8t/womens.png',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                size: 'large',
                 id: 4
             },
             {
                 title: 'mens',
-                imageUrl: 'htttps://i.ibb.co/R70vBrQ/men.png',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                size: 'large',
                 id: 5
             }]
         }
@@ -40,8 +42,8 @@ class Directory extends React.Component {
     render () {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, id}) => (
-                    <MenuItem key={id} title={title} />
+                {this.state.sections.map(({ title, imageUrl, id, size}) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                 ))}
             </div>
         )
