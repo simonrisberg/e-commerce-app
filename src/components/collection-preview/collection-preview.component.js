0ac8761f697c117a242c3edc9e4209c7b520bc0f
Simon Risberg <sirsimon40@gmail.com>
2020-05-03 12:13:12 +0200

    Did an additional save

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -7,7 +7,9 @@ const CollectionPreview = ({ title, items }) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((item, index) => index < 4).map((item) => (
+                items
+                .filter((item, index) => index < 4)
+                .map((item) => (
                     <div key={item.id}>{item.name}</div>
                 ))
             }
