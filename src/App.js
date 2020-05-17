7a6dfb2f8b9f7d948508b19a91c02a71d38a9d5c
Simon Risberg <sirsimon40@gmail.com>
2020-05-17 18:26:18 +0200

    Created a sign in page as well as a start for a sign in component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
+        <Route path='/signin' component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
     
