1bdd29e8629b5c84d7e3d9c530e749feefe93203
Simon Risberg <sirsimon40@gmail.com>
2020-05-24 11:46:37 +0200

    Removed duplicate labels

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -41,7 +41,6 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange}
                         required 
                     />
-                    <label>Email</label>
                     <FormInput 
                         name='password' 
                         type='password' 
@@ -50,8 +49,6 @@ class SignIn extends React.Component {
                         label='password'
                         required 
                     />
-                    <label>Password</label>
-
                     <input type='submit' value='Submit Form' />
                 </form>
             </div>
