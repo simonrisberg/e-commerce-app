8a709b3fe0b62950afd7c34746ff65156802ffbb
Simon Risberg <sirsimon40@gmail.com>
2020-05-29 16:15:12 +0200

    Added a Google sign in button and fixed padding on the sign in form

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -52,8 +52,11 @@ class SignIn extends React.Component {
                         label='password'
                         required 
                     />
-                    <CustomButton type='submit' value='Submit Form'> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
+                    <div className='buttons'>
+                      <CustomButton type='submit' value='Submit Form'> Sign in </CustomButton>
+                      <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+                    </div>
+                    
                 </form>
             </div>
         )
