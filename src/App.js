97b50777a63f1878b7b4fa512e915d04ffc91e97
Simon Risberg <sirsimon40@gmail.com>
2020-05-29 13:22:45 +0200

    Added functionality that sets the logged in user on the app's state and removed it from the state when the component unmounts

--- src/App.js
+++ src/App.js
@@ -7,20 +7,41 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-  
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route path='/shop' component={ShopPage}></Route>
-        <Route path='/signin' component={SignInAndSignUpPage}></Route>
-      </Switch>
-    </div>
+class App extends React.Component {
+  constructor() {
+    super();
     
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribefromAuth = null
+
+  componentDidMount() {
+    this.unsubscribefromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribefromAuth();
+  }
+  
+  render () {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route path='/shop' component={ShopPage}></Route>
+          <Route path='/signin' component={SignInAndSignUpPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
