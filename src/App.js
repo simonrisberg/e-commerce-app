4cacf25c53e1f2a725899fe50af08d1b865aeab4
Simon Risberg <sirsimon40@gmail.com>
2020-05-30 15:00:01 +0200

    A logged in user is now also put on the app's state

--- src/App.js
+++ src/App.js
@@ -21,8 +21,22 @@ class App extends React.Component {
   unsubscribefromAuth = null
 
   componentDidMount() {
-    this.unsubscribefromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribefromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+        console.log(this.state);
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
