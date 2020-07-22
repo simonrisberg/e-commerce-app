dcc5913d78b631372aeeaa3ef078dff321b76610
Simon Risberg <sirsimon40@gmail.com>
2020-07-22 16:01:20 +0200

    Created a cart component, gave it a reducer, action and type as well as implemented it into the header component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,12 +4,13 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -33,11 +34,15 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        {
+          hidden ? null : <CartDropDown />
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+  hidden: state.cart.hidden
 });
 
 export default connect(mapStateToProps)(Header); 
