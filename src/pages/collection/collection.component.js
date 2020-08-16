6c7f7fc498aa3968d7f8b648c688fb6b3f0244ba
Simon Risberg <sirsimon40@gmail.com>
2020-08-16 12:50:54 +0200

    Changed the name of the Category Page to Collection Page

---
+++ src/pages/collection/collection.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => {
+  return (
+    <div className='collection-page'>
+      <h2>COLLECTION PAGE</h2>
+    
+    </div>
+)};
+
+export default CollectionPage;
