d65722e096e8cdf47dc5f3225311ee584968cb1d
Simon Risberg <sirsimon40@gmail.com>
2020-08-16 16:28:47 +0200

    The collection component now takes the appropriate collection items which is displayed based on the url params

--- src/pages/collection/collection.component.js
+++ src/pages/collection/collection.component.js
@@ -4,13 +4,20 @@ import { connect } from 'react-redux';
 
 import { selectCollection } from '../../redux/shop/shop.selector';
 
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
     <div className='collection-page'>
-      <h2>COLLECTION PAGE</h2>
-    
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {
+          items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
     </div>
 )};
 
