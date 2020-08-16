a6e93fe54e347b1d93c4cbbf0273e98943b4f3fa
Simon Risberg <sirsimon40@gmail.com>
2020-08-16 12:45:55 +0200

    Created a category page and implemented routes in the shop page component

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { Route } from 'react-router-dom';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => {
+  return(
       <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
       </div> 
-);
+)};
 
 export default ShopPage;
