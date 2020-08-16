7a0f1a56079f00b642b00f99420cf01c5a913aa0
Simon Risberg <sirsimon40@gmail.com>
2020-08-16 17:25:30 +0200

    Changed the shop data from array to object and made the additional changes in the shop selector

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,14 +2,6 @@ import { createSelector } from 'reselect';
 
 import memoize from 'lodash.memoize';
 
-const COLLECION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -20,6 +12,6 @@ export const selectCollections = createSelector(
 export const selectCollection = memoize((collectionUrlParam) => 
   createSelector(
     [selectCollections],
-    collections => collections.find(collection  => collection.id === COLLECION_ID_MAP[collectionUrlParam]) 
+    collections => collections[collectionUrlParam]
   )
 );  
