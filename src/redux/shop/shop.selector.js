be805ad69dc678f39141f55fe9e389fdbd4a63f0
Simon Risberg <sirsimon40@gmail.com>
2020-08-22 17:28:30 +0200

    Created a new collections selector that filters out the key values due to me changing it from an array to an object

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -7,6 +7,11 @@ const selectShop = state => state.shop;
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
 )
 
 export const selectCollection = memoize((collectionUrlParam) => 
